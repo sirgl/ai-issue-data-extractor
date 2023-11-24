b4208c7548b230c15000f8e69886596247b6d4bf
Roman Ivanov <roman.ivanov@jetbrains.com>
2023-11-24 12:25:22 +0100

    uploaded all the descriptions

--- yt-ai/src/main/groovy/org/example/Main.groovy
+++ yt-ai/src/main/groovy/org/example/Main.groovy
@@ -1,9 +1,22 @@
 package org.example
 
+import com.knuddels.jtokkit.Encodings
+import com.knuddels.jtokkit.api.Encoding
+import com.knuddels.jtokkit.api.EncodingRegistry
+import com.knuddels.jtokkit.api.ModelType
 import com.theokanning.openai.completion.chat.ChatCompletionRequest
 import com.theokanning.openai.completion.chat.ChatMessage
+import com.theokanning.openai.embedding.Embedding
+import com.theokanning.openai.embedding.EmbeddingRequest
+import com.theokanning.openai.embedding.EmbeddingResult
 import com.theokanning.openai.service.OpenAiService
 import groovy.json.JsonSlurper
+import io.pinecone.PineconeClient
+import io.pinecone.PineconeClientConfig
+import io.pinecone.PineconeConnection
+import io.pinecone.proto.UpsertRequest
+import io.pinecone.proto.UpsertResponse
+import io.pinecone.proto.Vector
 import util.CSVExtractor
 import data.Issue
 
@@ -12,106 +25,66 @@ import java.time.Duration
 
 static void main(String[] args) {
     def apiKey = args[0]
-    def data = CSVExtractor.extract("data.csv")
-    OpenAiService service = new OpenAiService(apiKey, Duration.ofSeconds(30))
+    def pinecodeKey = args[1]
+    def data = CSVExtractor.extract("data.csv").parallelStream().filter {countTokens(getTextForIssue(it)) < 8192 }.toList()
+    OpenAiService openAiService = new OpenAiService(apiKey, Duration.ofSeconds(30))
 
-    def sb = new StringBuilder()
-    for (final def issue in data.take(10)) {
-        try {
-            def result = handleSingle(issue, service)
-            sb.append("${result.issue.id()},${result.response.subsystem()},${result.response.androidPluginInvolved()}\n")
-        } catch (def e) {
-            e.printStackTrace()
-        }
+    PineconeClientConfig config = new PineconeClientConfig()
+            .withApiKey(pinecodeKey)
+            .withEnvironment("us-east4-gcp")
+            .withProjectName("463dbdd")
+            .withServerSideTimeoutSec(10);
+
+
+    PineconeClient client = new PineconeClient(config)
+    def pineconeConnection = client.connect("hackathon-2023")
+
+
+    indexIssues(data, openAiService, pineconeConnection)
+}
+
+private static void indexIssues(List<Issue> issues, OpenAiService openAiService, PineconeConnection pineconeConnection) {
+    def rawIssues = issues.collect { getTextForIssue(it) }
+
+    def embeddings = openAiService.createEmbeddings(new EmbeddingRequest("text-embedding-ada-002", rawIssues, "user"))
+
+    def issuesWithEmbeddings = embeddings.data.withIndex().collect {
+        def vectorOfFloats = it.v1.embedding.collect { it as Float }
+
+        def index = it.v2
+        def vector = Vector.newBuilder()
+                .addAllValues(vectorOfFloats)
+                .setId(issues[index].id())
+                .build()
+        new IssueWithEmbedding(embedding: vector, issue: issues[index])
     }
-    def file = new File("out.csv")
-    file << sb.toString()
-}
 
-static Result handleSingle(Issue issue, OpenAiService service) {
-    ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
-            .builder()
-            .messages(prompt(issue))
-            .maxTokens(256)
-            .model("gpt-4-vision-preview")
-            .build()
-    def rawResponse = service.createChatCompletion(chatCompletionRequest).choices
-            .collect { it.message.content }
-            .join("")
-    if (rawResponse.substring(0, 7) == "```json") {
-        rawResponse = rawResponse[7..-4]
+    def chunks = issuesWithEmbeddings.collate(30)
+    for (final def chunk in chunks) {
+        UpsertRequest hybridRequest = UpsertRequest.newBuilder()
+                .setNamespace("")
+                .addAllVectors(chunk.collect { it.embedding })
+                .build()
+        def upsertResponse = pineconeConnection.blockingStub.upsert(hybridRequest)
+        println upsertResponse.toString()
     }
-    def response = new JsonSlurper().parseText(rawResponse) as Response
-    return new Result(issue: issue, response: response)
+
+
 }
 
-static List<ChatMessage> prompt(Issue issue) {
-    def systemPrompt = """
-You are autonomous agent helping IntelliJ Support Engineer.
-You don't have access to the internet or human.
-You may only emit JSON in form specified in the prompt.
-
-The list of available refactorings in Intellij idea:
-```
-Make Static
-Migrate
-Move and copy refactorings
-Pull members up, push members down
-Rename refactorings
-Replace inheritance with delegation
-Safe delete
-Type migration
-Use interface where possible
-Introduction to refactoring
-Remove middleman
-Wrap return value
-Convert Raw Types to Generics refactoring
-Replace constructor with builder
-Replace constructor with factory method
-Change Signature for Java
-Convert anonymous to inner
-Convert to Instance Method
-Encapsulate Fields
-Extract constant
-Extract field
-Extract interface
-Extract method
-Extract superclass
-Extract/Introduce variable
-Extract Parameter
-Extract into class refactorings
-Extract trait in Scala
-Replace Conditional Logic with Strategy Pattern
-Find and replace code duplicates
-Invert Boolean
-```
-"""
-
-    return [
-            new ChatMessage("system", systemPrompt),
-            new ChatMessage("user", """
-The summary of the issue:
-```
-${issue.summary()}
-```
-The issue is:
-```
-${issue.description()}
-```
-
-Response format (without ticks and markdown, raw json, must be immediately parseable):
-{
-  "subsystem": "<refactoring name from the list of options or None when none applicable or if the issue is not related to refactorings >",
-  "androidPluginInvolved": <true/false>
-}
-"""),
-    ]
+private static String getTextForIssue(Issue it) {
+    "${it.summary()}\n${it.description()}" as String
 }
 
-record Response(String subsystem, boolean androidPluginInvolved) {}
-
-
-class Result {
+class IssueWithEmbedding {
+    Vector embedding
     Issue issue
-    Response response
+}
+
+
+static int countTokens(String text) {
+    EncodingRegistry registry = Encodings.newDefaultEncodingRegistry();
+    Encoding encoding = registry.getEncodingForModel(ModelType.TEXT_EMBEDDING_ADA_002)
+    List<Integer> encoded = encoding.encode(text);
+    return encoded.size()
 }
---
+++ yt-ai/src/main/groovy/org/example/RefactoringsClassification.groovy
@@ -0,0 +1,124 @@
+package org.example
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest
+import com.theokanning.openai.completion.chat.ChatMessage
+import com.theokanning.openai.service.OpenAiService
+import data.Issue
+import groovy.json.JsonSlurper
+import util.CSVExtractor
+
+import java.time.Duration
+
+
+static void main(String[] args) {
+    def apiKey = args[0]
+    def data = CSVExtractor.extract("data.csv")
+    handleRefactorings(apiKey, data)
+}
+
+
+private static void handleRefactorings(String apiKey, List<Issue> data) {
+    OpenAiService service = new OpenAiService(apiKey, Duration.ofSeconds(30))
+
+    def sb = new StringBuilder()
+    for (final def issue in data.take(10)) {
+        try {
+            def result = handleSingle(issue, service)
+            sb.append("${result.issue.id()},${result.response.subsystem()},${result.response.androidPluginInvolved()}\n")
+        } catch (def e) {
+            e.printStackTrace()
+        }
+    }
+    def file = new File("out.csv")
+    file << sb.toString()
+}
+
+
+
+static Result handleSingle(Issue issue, OpenAiService service) {
+    ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+            .builder()
+            .messages(prompt(issue))
+            .maxTokens(256)
+            .model("gpt-4-vision-preview")
+            .build()
+    def rawResponse = service.createChatCompletion(chatCompletionRequest).choices
+            .collect { it.message.content }
+            .join("")
+    if (rawResponse.substring(0, 7) == "```json") {
+        rawResponse = rawResponse[7..-4]
+    }
+    def response = new JsonSlurper().parseText(rawResponse) as Response
+    return new Result(issue: issue, response: response)
+}
+
+static List<ChatMessage> prompt(Issue issue) {
+    def systemPrompt = """
+You are autonomous agent helping IntelliJ Support Engineer.
+You don't have access to the internet or human.
+You may only emit JSON in form specified in the prompt.
+
+The list of available refactorings in Intellij idea:
+```
+Make Static
+Migrate
+Move and copy refactorings
+Pull members up, push members down
+Rename refactorings
+Replace inheritance with delegation
+Safe delete
+Type migration
+Use interface where possible
+Introduction to refactoring
+Remove middleman
+Wrap return value
+Convert Raw Types to Generics refactoring
+Replace constructor with builder
+Replace constructor with factory method
+Change Signature for Java
+Convert anonymous to inner
+Convert to Instance Method
+Encapsulate Fields
+Extract constant
+Extract field
+Extract interface
+Extract method
+Extract superclass
+Extract/Introduce variable
+Extract Parameter
+Extract into class refactorings
+Extract trait in Scala
+Replace Conditional Logic with Strategy Pattern
+Find and replace code duplicates
+Invert Boolean
+```
+"""
+
+    return [
+            new ChatMessage("system", systemPrompt),
+            new ChatMessage("user", """
+The summary of the issue:
+```
+${issue.summary()}
+```
+The issue is:
+```
+${issue.description()}
+```
+
+Response format (without ticks and markdown, raw json, must be immediately parseable):
+{
+  "subsystem": "<refactoring name from the list of options or None when none applicable or if the issue is not related to refactorings >",
+  "androidPluginInvolved": <true/false>
+}
+"""),
+    ]
+}
+
+record Response(String subsystem, boolean androidPluginInvolved) {}
+
+
+class Result {
+    Issue issue
+    Response response
+}
